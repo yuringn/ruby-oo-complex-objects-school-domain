db66aa0438f1d98c99865353afa3e1630c1ec0c0
Yuri Nguyen <yuringuyen312@gmail.com>
2021-02-23 22:08:41 -0500

    commit tree 567e15dc3b389da623fc1a29c283a56abdb7e159

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+require"pry"
+class School
+    attr_accessor :roster, :name
+   def initialize(name)
+    @roster = {}
+    @name = name
+   end
+   def add_student(name,grade)
+      @roster[grade] ||=[]
+      @roster[grade] << name
+    end
+   def grade(grade)
+    @roster[grade]
+   end
+   def sort
+    @roster.each {|grade,name| name.sort!}
+        @roster
+   end
+end
+
